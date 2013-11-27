f50cb7b39e146158e50a3b2cfd86282ea1b6d7a3
micahmoral <micahmoral@yahoo.com>
2013-11-27 15:12:01 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,17 +1,31 @@
 <html>
 	<body>
 			<center>
+		<h1>Messages</h1>
+		
+<table border = '1'>
+		<tr>
+			<th>Name</th>
+			<th>Email</th>
+			<th>Message</th>
+			<th>Date Approved</th>
+		</tr>
+</table><br><br>
+
+
 		<h1>Post New Message</h1>
 
 		<form action = "postmessage.php" method = "post" >
 
-		Name <input type = "text" name = "name"><br><br>
-		Email <input type = "text" name = "email"><br><br>
-		Message<br><br>
+		Name:<input type = "text" name = "name"><br><br>
+		Email:<input type = "text" name = "email"><br><br>
+		Message:<br><br>
 		<textarea name = "message"></textarea><br><br>
 		<input type = "submit" value = "Post Message">
+		<input type = "submit" value = "View Messages">
 		
+
 	</body>
-</form>
+</form>																																																																																																																																																						
 </center>
-</html>																																																																																																																																																																														
+</html>			
