55f219dcb6b3f3e77b7db57a46cea5ccac0ef08f
micahmoral <micahmoral@yahoo.com>
2013-11-27 08:54:15 +0800

    Create home.php

---
+++ home.php
@@ -0,0 +1,17 @@
+<html>
+	<body>
+			<center>
+		<h1>Post New Message</h1>
+
+		<form action = "postmessage.php" method = "post" >
+
+		Name <input type = "text" name = "name"><br><br>
+		Email <input type = "text" name = "email"><br><br>
+		Message<br><br>
+		<textarea name = "message"></textarea><br><br>
+		<input type = "submit" value = "Post Message">
+		
+	</body>
+</form>
+</center>
+</html>																																																																																																																																																																														
